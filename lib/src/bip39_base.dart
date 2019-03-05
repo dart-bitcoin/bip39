e7cd01c76af0f4c5df8cc5f65930ce54b05f1da9
Matthew Cornelisse <squarerootofnegativeone@gmail.com>
2019-03-05 08:08:44 -0600

    Added multi  lingual support.
    - Added support for any language.
    - Defaults to English
    - Added all official word lists that use roman alphabet.

--- lib/src/bip39_base.dart
+++ lib/src/bip39_base.dart
@@ -4,7 +4,9 @@ import 'dart:typed_data';
 import 'package:crypto/crypto.dart' show sha256;
 import 'package:hex/hex.dart';
 import 'utils/pbkdf2.dart';
-import 'wordlists/english.dart';
+import 'wordlists/all.dart';
+
+const String _defaultLanguage="english";
 const int _SIZE_BYTE = 255;
 const _INVALID_MNEMONIC = 'Invalid mnemonic';
 const _INVALID_ENTROPY = 'Invalid entropy';
@@ -47,15 +49,16 @@ Uint8List _randomBytes(int size) {
 }
 String generateMnemonic({
   int strength = 128,
-  RandomBytes randomBytes = _randomBytes
+  RandomBytes randomBytes = _randomBytes,
+  String language=_defaultLanguage
 }) {
   assert(strength % 32 == 0);
   final entropy = randomBytes(strength ~/ 8);
-  return entropyToMnemonic(HEX.encode(entropy));
+  return entropyToMnemonic(HEX.encode(entropy),language: language);
 }
-String entropyToMnemonic(String entropyString) {
+String entropyToMnemonic(String entropyString,{String language=_defaultLanguage}) {
   final entropy = HEX.decode(entropyString);
-  if (entropy.length < 16) {
+  if (entropy.length < 4) {
     throw ArgumentError(_INVALID_ENTROPY);
   }
   if (entropy.length > 32) {
@@ -72,7 +75,7 @@ String entropyToMnemonic(String entropyString) {
       .allMatches(bits)
       .map((match) => match.group(0))
       .toList(growable: false);
-  List<String> wordlist = WORDLIST;
+  List<String> wordlist = WORDLIST[language];
   String words = chunks.map((binary) => wordlist[_binaryToByte(binary)]).join(' ');
   return words;
 }
@@ -85,20 +88,20 @@ String mnemonicToSeedHex(String mnemonic) {
     return byte.toRadixString(16).padLeft(2, '0');
   }).join('');
 }
-bool validateMnemonic(String mnemonic) {
+bool validateMnemonic(String mnemonic,{String language=_defaultLanguage}) {
   try {
-    mnemonicToEntropy(mnemonic);
+    mnemonicToEntropy(mnemonic,language: language);
   } catch (e) {
     return false;
   }
   return true;
 }
-String mnemonicToEntropy (mnemonic) {
+String mnemonicToEntropy (mnemonic,{String language=_defaultLanguage}) {
   var words = mnemonic.split(' ');
   if (words.length % 3 != 0) {
     throw new ArgumentError(_INVALID_MNEMONIC);
   }
-  final wordlist = WORDLIST;
+  final wordlist = WORDLIST[language];
     // convert word indices to 11 bit binary strings
     final bits = words.map((word) {
       final index = wordlist.indexOf(word);
@@ -118,7 +121,7 @@ String mnemonicToEntropy (mnemonic) {
       .allMatches(entropyBits)
       .map((match) => _binaryToByte(match.group(0)))
       .toList(growable: false));
-  if (entropyBytes.length < 16) {
+  if (entropyBytes.length < 4) {
     throw StateError(_INVALID_ENTROPY);
   }
   if (entropyBytes.length > 32) {
--- lib/src/wordlists/english.dart
+++ lib/src/wordlists/english.dart
@@ -1,4 +1,4 @@
-const WORDLIST = [
+const List<String> WORDLIST = [
   "abandon",
   "ability",
   "able",
