8499b4415cfed0f090da25d47284c6f86829a3d6
poka <poka@p2p.legal>
2021-12-09 08:30:24 +0100

    Remove unused files

--- example/bip39_example.dart
+++ example/bip39_example.dart
@@ -1,4 +1,4 @@
-import 'package:bip39_multi/bip39_multi.dart' as bip39;
+import 'package:bip39_multi/bip39.dart' as bip39;
 
 main() async {
   String randomMnemonic = bip39.generateMnemonic();
--- lib/bip39_multi.dart
+++
@@ -1,9 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library bip39_multi;
-
-export 'src/bip39_multi_base.dart';
-export 'src/wordlists/all.dart';
-
-// TODO: Export any libraries intended for clients of this package.
